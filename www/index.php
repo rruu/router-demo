360bc11fbfe8a6410b4ce1a84b943d949d6159b0
ru <git@xoyo.ru>
2018-03-26 14:28:20 +0300

    add one

--- www/index.php
+++ www/index.php
@@ -1,13 +1,53 @@
-<?php @include ('connect.php'); ?>
-<?php require_once('session.php'); ?>
+<?php @include 'connect.php' ?>
+<?php require_once('session.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome!</title>
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<title>Demo router</title>
 </head>
 <body>
-	<h1>Welcome to php-membership!</h1>
-	<p><a href="members.php">Members-only area</a></p>
-	<p><a href="login.php">Login</a> or <a href="register.php">Register an user</a>.</p>
-</body>
+<div class="container">
+    <div class="row">
+		<div class="span12">
+
+
+			<form class="form-horizontal" action='login-user.php' method="POST">
+			  <fieldset>
+			    <div id="legend">
+			      <legend class="">Login</legend>
+			    </div>
+			    <div class="control-group">
+			      <!-- Username -->
+			      <label class="control-label"  for="username">Username</label>
+			      <div class="controls">
+			        <input type="text" id="username" name="Username" placeholder="" class="input-xlarge">
+			      </div>
+			    </div>
+			    <div class="control-group">
+			      <!-- Password-->
+			      <label class="control-label" for="password">Password</label>
+			      <div class="controls">
+			        <input type="password" id="password" name="Password" placeholder="" class="input-xlarge">
+			      </div>
+			    </div>
+			    <div class="control-group">
+			      <!-- Button -->
+			      <div class="controls">
+			        <button class="btn btn-success">Login</button>
+			      </div>
+			    </div>
+			  </fieldset>
+			</form>
+
+
+
+        </div>
+	</div>
+</div>
+
+
+
 </html>
